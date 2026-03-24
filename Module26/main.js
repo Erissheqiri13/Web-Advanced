9056f01f597d8398f51a2e0f484dc433cfa04361
Erissheqiri13 <erissheqiri8@gmail.com>
2026-03-24 19:19:21 +0100

    a

---
+++ Module26/main.js
@@ -0,0 +1,61 @@
+console.log("hello")
+
+var x = 5 * 4727;
+
+console.log(x);
+
+var a = 50
+var b = 10
+var c = a + b
+console.log(c)
+
+var d = 50
+var e = 10
+var f = a - b
+console.log(f)
+
+var g = 50
+var h = 10
+var i = a % b
+console.log(i)
+
+var j = 50
+var k = 10
+var l = a / b
+console.log(l)
+
+var m = 99
+m++;
+console.log(m)
+
+var n = 99
+n--;
+console.log(n)
+
+var o = 99
+var p = o** 2;
+console.log(p)
+
+var q = 15
+q += 5;
+console.log(q)
+
+var r = 15
+r -= 5;
+console.log(r)
+
+var s = 15
+s /= 5;
+console.log(s)
+
+var t = 5;
+var u = "88";
+
+console.log(t > u);
+console.log(t < u);
+console.log(t === u);
+
+var fname = "filan";
+var surname = "fisteku";
+
+console.log(fname+" "+surname);
