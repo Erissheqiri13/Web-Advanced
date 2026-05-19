03b4991e6a31f8ec088b63d6fd9efb75485b4311
Erissheqiri13 <erissheqiri8@gmail.com>
2026-05-19 19:12:18 +0200

    a

---
+++ Module37/main.js
@@ -0,0 +1,21 @@
+// $(function(){
+//     alert("hello")
+// })
+
+$('#sort').sortable()
+
+$('#accordion').accordion()
+
+$(function(){
+    $('#accordion').accordion({
+        collapsible: true
+    })
+})
+
+$('#tabs').tabs()
+
+$(function(){
+    $('#tabs').tabs({
+        collapsible: true
+    })
+})
